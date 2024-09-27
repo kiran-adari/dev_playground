45e874c504a8ef02fc3f54f710a5db44b663f8d0
Kiran Adari <adarikiran1@gmail.com>
2024-09-27 19:28:24 +0530

    query sum of an array both brute force and optimised approach.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello all welcome to code playground!");
+
+        QuerySum obj = new QuerySum();
+        obj.optimised_approach();
     }
 }
